9c3efbe90dad26bcf3ce42089ff2b4634f459acb
Vince Elizaga <vncelizaga@gmail.com>
2023-09-17 12:47:44 -0400

    implement mqtt connections

--- src/components/JoystickControllers.tsx
+++ src/components/JoystickControllers.tsx
@@ -1,10 +1,55 @@
 import { Joystick } from "react-joystick-component";
 import { IonCol, IonGrid, IonRow } from "@ionic/react";
 import "./JoystickController.css";
+import * as mqtt from "mqtt";
+
+const mqttTopic = "rovy/motor/c7de5c35-3882-48bc-bac6-f67fcf50da0f";
 
 interface ContainerProps {}
 
-const ExploreContainer: React.FC<ContainerProps> = () => {
+const JoystickControllers: React.FC<ContainerProps> = () => {
+  const clientId = "mqttjs_" + Math.random().toString(16).substr(2, 8);
+
+  const host = "ws://54.169.163.170:8083/mqtt";
+
+  const options: any = {
+    keepalive: 60,
+    clientId: clientId,
+    protocolId: "MQTT",
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    will: {
+      topic: "WillMsg",
+      payload: "Connection Closed abnormally..!",
+      qos: 0,
+      retain: false,
+    },
+  };
+
+  const client = mqtt.connect(host, options);
+
+  client.on("connect", () => {
+    console.log("connected");
+  });
+
+  client.on("message", (topic, message) => {
+    // message is Buffer
+    console.log(message.toString());
+  });
+
+  const steerControl = (data: any) => {
+    client.publish(mqttTopic, `${data.x},${data.y}`);
+
+    console.log(data);
+  };
+
+  const steerStop = (data: any) => {
+    console.log(data);
+    client.publish(mqttTopic, `0,0`);
+  };
+
   return (
     <>
       <IonGrid class="joystick-container-bottom">
@@ -14,8 +59,9 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
               baseColor="gray"
               stickColor="lightgray"
               size={125}
-              move={() => {}}
-              stop={() => {}}
+              move={steerControl}
+              stop={steerStop}
+              throttle={100}
             ></Joystick>
           </IonCol>
           <IonCol class="joystick-align-right">
@@ -33,4 +79,4 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   );
 };
 
-export default ExploreContainer;
+export default JoystickControllers;
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import legacy from "@vitejs/plugin-legacy";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import nodePolyfills from "rollup-plugin-polyfill-node";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), legacy()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: "jsdom",
@@ -12,5 +14,28 @@ export default defineConfig({
   },
   define: {
     "process.env": process.env,
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+          process: true,
+        }),
+      ],
+    },
+  },
+  build: {
+    target: "es2020", // 👈 build.target,
+    rollupOptions: {
+      // Enable rollup polyfills plugin
+      // used during production bundling
+      plugins: [nodePolyfills()],
+    },
   },
 });
